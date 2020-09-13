b8f2c473878f3e3a21ff4aa4e8ac445da17d352c
SKhamkar07 <shashank.khamkar@gmail.com>
2020-09-13 14:07:10 +0530

    End of Section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,41 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
   // tslint:disable-next-line: typedef
-  ngOnInit() {
+  ngOnInit() {}
+
+  // tslint:disable-next-line: typedef
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Login successful');
+      },
+      (error) => {
+        this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(['/members']);
+      }
+    );
   }
 
   // tslint:disable-next-line: typedef
-  login()
-  {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('Login successful');
-    }, error => {
-      this.alertify.error(error);
-    });
-  }
-
-  // tslint:disable-next-line: typedef
-  loggedIn()
-  {
+  loggedIn() {
     return this.authService.loggdedIn();
   }
 
   // tslint:disable-next-line: typedef
-  logOut()
-  {
+  logOut() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
